7b898c8d7d92d86f1eb416840948433e3ac6e62d
Kevin Jennison <kevin.jennison1@gmail.com>
2020-10-08 15:43:58 -0700

    Remove console log

--- examples/with-firebase-authentication-ssr/pages/api/login-v2.js
+++ examples/with-firebase-authentication-ssr/pages/api/login-v2.js
@@ -1,9 +1,7 @@
 import { setAuthCookies } from 'next-firebase-auth'
 
 const handler = async (req, res) => {
-  const { idToken, refreshToken, AuthUser } = await setAuthCookies(req, res)
-  console.log('login result:', idToken, refreshToken, AuthUser)
-
+  await setAuthCookies(req, res)
   return res.status(200).json({ status: true })
 }
 

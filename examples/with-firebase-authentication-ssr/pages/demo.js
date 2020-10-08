56d0a8e23c9f0b17004a69877a59f39315543549
Kevin Jennison <kevin.jennison1@gmail.com>
2020-10-08 11:39:29 -0700

    Set some next-firebase-auth config

--- examples/with-firebase-authentication-ssr/pages/demo.js
+++ examples/with-firebase-authentication-ssr/pages/demo.js
@@ -8,12 +8,27 @@ import {
 
 const isServerSide = typeof window === 'undefined'
 initAuth({
-  onAuthStateChanged: () => {}, // TODO
-  authRequiredRedirectURL: '/auth',
-  appRedirectURL: '/demo',
+  // onAuthStateChanged: () => {},
+  // authRequiredRedirectURL: '/auth',
+  // appRedirectURL: '/demo',
   // Don't set the Firebase admin config on the client side.
-  ...(isServerSide && { firebaseAdminInitConfig: { some: 'stuff' } }), // TODO
-  firebaseClientInitConfig: {}, // TODO
+  ...(isServerSide && {
+    firebaseAdminInitConfig: {
+      credential: {
+        projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+        // https://stackoverflow.com/a/41044630/1332513
+        privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+      },
+      databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+    },
+  }),
+  firebaseClientInitConfig: {
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_PUBLIC_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  },
 })
 
 const Demo = (props) => {
